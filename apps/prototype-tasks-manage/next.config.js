b675e94564c8d350921be479fd97fc64c5a624a8
Kames <info@kamescg.com>
2020-08-03 12:38:59 -0700

    ⚡

--- apps/prototype-tasks-manage/next.config.js
+++ apps/prototype-tasks-manage/next.config.js
@@ -1,6 +1,17 @@
+const withPlugins = require("next-compose-plugins");
 const withTM = require("next-transpile-modules")([
   "react-monday",
   "react-monday-ui",
 ]);
 
-module.exports = withTM();
+// module.exports = ;
+
+// module.exports = {
+//   target: "serverless",
+//   // Other options
+//   withTM()
+// };
+
+module.exports = withPlugins([withTM], {
+  target: "serverless",
+});
